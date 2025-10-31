90b307a3db6fd4c4e2919b9f3fe48c8cbc5435b7
Charles Mc Vigo <charlesvigo8@gmail.com>
2025-10-31 23:43:40 +0800

    modified

--- daily-report-aventus/src/pages/Homepage.jsx
+++ daily-report-aventus/src/pages/Homepage.jsx
@@ -5,7 +5,7 @@ const Homepage = () => {
     <div className="mx-auto flex flex-col justify-center items-center h-screen w-full gap-8">
       <img src=".\src\assets\background-image.png" alt="Aventus background" />
       <h1 className="font-mono px-5 text-3xl tracking-widest">Daily Report</h1>
-      <button className="btn btn-outline btn-success btn-wide">Get started!</button>
+      <button className="btn btn-outline btn-success btn-wide   ">Get started!</button>
     </div>
   );
 };
