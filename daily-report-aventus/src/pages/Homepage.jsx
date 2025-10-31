fdb5a21df03f843a728d4a226a8a0b7d2534fb19
Charles Mc Vigo <charlesvigo8@gmail.com>
2025-10-31 23:54:05 +0800

    modified images and logo

--- daily-report-aventus/src/pages/Homepage.jsx
+++ daily-report-aventus/src/pages/Homepage.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Homepage = () => {
   return (
     <div className="mx-auto flex flex-col justify-center items-center h-screen w-full gap-8">
-      <img src=".\src\assets\background-image.png" alt="Aventus background" />
+      <img src="/background-image.png" alt="Aventus background" />
       <h1 className="font-mono px-5 text-3xl tracking-widest">Daily Report</h1>
       <button className="btn btn-outline btn-success btn-wide   ">Get started!</button>
     </div>
