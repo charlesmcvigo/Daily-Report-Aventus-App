90623f85b6c5c7637d3bb2462714de4d7ce0404c
Charles Mc Vigo <charlesvigo8@gmail.com>
2025-11-01 00:06:52 +0800

    React notification added

--- daily-report-aventus/src/pages/Homepage.jsx
+++ daily-report-aventus/src/pages/Homepage.jsx
@@ -1,11 +1,20 @@
 import React from "react";
+import toast from "react-hot-toast";
 
 const Homepage = () => {
   return (
-    <div className="mx-auto flex flex-col justify-center items-center h-screen w-full gap-8">
-      <img src="/background-image.png" alt="Aventus background" />
-      <h1 className="font-mono px-5 text-3xl tracking-widest">Daily Report</h1>
-      <button className="btn btn-outline btn-success btn-wide   ">Get started!</button>
+    <div className="mx-auto flex flex-col justify-center items-center h-screen w-full gap-8 px-4">
+      <img
+        src="/background-image.png"
+        alt="Aventus background"
+        className="w-full max-w-lg h-auto object-contain"
+      />
+      <h1 className="font-mono px-5 text-3xl tracking-widest text-center">
+        Daily Report
+      </h1>
+      <button onClick={()=> toast.error("Di pa tapos! Atat yarn?")} className="btn btn-outline btn-success btn-wide">
+        Get started!
+      </button>
     </div>
   );
 };
